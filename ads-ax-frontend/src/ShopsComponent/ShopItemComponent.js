a607da46e6ae3a83b02ae045e16e5dd506a55306
dylanterrell <dylan.terrell14@stjohns.edu>
2018-09-28 01:44:03 -0400

    Shops Page Update

--- ads-ax-frontend/src/ShopsComponent/ShopItemComponent.js
+++ ads-ax-frontend/src/ShopsComponent/ShopItemComponent.js
@@ -1,16 +1,95 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
-const ShopItemComponent = ({item}) => {
-  return (
-    <Grid fluid>
-      <Row center="xs">
+class ShopItemComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shopAccount: 'Authentic Fashion House',
+      displayName: 'AUTHENTIC',
+      hq: 'New York City, NY',
+      description: 'A High-Class Private Label by Authentic',
+      contactEmail: 'dylan@authentic.shop',
+      contactName: 'Dylan Terrell',
+      contactPhone: '770-990-5090',
+      contactTitle: 'Owner',
+      attachments: [],
+      displayNameState: true,
+      hqState: true,
+      descriptionState: true,
+      emailState: true,
+      nameState: true,
+      phoneState: true,
+      titleState: true,
+      attachmentsState: true
+      }
+
+    this.handleChange = this.handleChange.bind(this);
+    this._save = this._save.bind(this);
+    this._click = this._click.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  _click(item) {
+    this.setState(prevState => ({ [item]: !prevState[item] }));
+  }
+
+  _save(event, item) {
+    if(!this.state[item]) {
+      // fire patch request method from container prop
+      console.log(event.target.name);
+      console.log(item);
+      this._click(item);
+    }
+    else {
+      console.log("read only is locked on this element");
+    }
+  }
+
+  render() {
+    return (
         <div className="shop-item">
-          <h1>ITEM</h1>
+        <h4>Authentic Fashion House</h4>
+          <Row>
+            <Col xs={6} sm={6} md={3} lg={3} className="col-element">
+              <h5>Display Name <a onClick={() => this._click("displayNameState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="displayName" name="displayName" value={this.state.displayName} readOnly={this.state.displayNameState} onChange={this.handleChange} onBlur={(event) => this._save(event, "displayNameState")}/>
+            </Col>
+            <Col xs={6} sm={6} md={3} lg={3} className="col-element">
+              <h5>Description <a onClick={() => this._click("descriptionState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="description" name="description" value={this.state.description} readOnly={this.state.descriptionState} onChange={this.handleChange} onBlur={(event) => this._save(event, "descriptionState")}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} sm={6} md={3} lg={3} className="col-element">
+              <h5>HQ <a onClick={() => this._click("hqState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="hq" name="hq" value={this.state.hq} readOnly={this.state.hqState} onChange={this.handleChange} onBlur={(event) => this._save(event, "hqState")}/>
+            </Col>
+            <Col xs={6} sm={6} md={3} lg={3} className="col-element">
+              <h5>Contact Name <a onClick={() => this._click("nameState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="contactName" name="contactName" value={this.state.contactName} readOnly={this.state.nameState} onChange={this.handleChange} onBlur={(event) => this._save(event, "nameState")}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} sm={6} md={3} lg={3} className="col-element">
+              <h5>Attachments <a onClick={() => this._click("attachmentsState")} href="#" className="edit-item">Edit</a></h5>
+            </Col>
+            <Col xs={6} sm={6} md={3} lg={3} className="col-element">
+              <h5>Title <a onClick={() => this._click("titleState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="contactTitle" name="contactTitle" value={this.state.contactTitle} readOnly={this.state.titleState} onChange={this.handleChange} onBlur={(event) => this._save(event, "titleState")}/>
+              <h5>Phone <a onClick={() => this._click("phoneState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="contactPhone" name="contactPhone" value={this.state.contactPhone} readOnly={this.state.phoneState} onChange={this.handleChange} onBlur={(event) => this._save(event, "phoneState")}/>
+              <h5>Email <a onClick={() => this._click("emailState")} href="#" className="edit-item">Edit</a></h5>
+              <input className="item-element" type="text" id="contactEmail" name="contactEmail" value={this.state.contactEmail} readOnly={this.state.emailState} onChange={this.handleChange} onBlur={(event) => this._save(event, "emailState")}/>
+            </Col>
+          </Row>
         </div>
-      </Row>
-    </Grid>
-  );
+    );
+  }
 }
 
 export default ShopItemComponent;
