448e67de2708e6edf17f97af3e492765dcd920ab
dylanterrell <dylan.terrell14@stjohns.edu>
2018-10-12 13:45:01 -0400

    Updated business logic for delete api

--- ads-ax-backend-service/src/shops-api-service/deleteShopHandler.js
+++ ads-ax-backend-service/src/shops-api-service/deleteShopHandler.js
@@ -5,7 +5,7 @@ module.exports.deleteShop = async (event, context, callback) => {
   const params = {
     TableName: "Shops",
     Key: {
-      shopAccount: event.pathParameters.shopAccount
+      shopAccount: decodeURI(event.pathParameters.shopAccount)
     }
   }
 
